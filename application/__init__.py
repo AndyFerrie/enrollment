7ac15d67c4c1d81c8ab4032810b20f973a16f671
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-11-13 08:11:01 +0000

    Removes flask restplus

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
-from flask_restplus import Api
 
 from config import Config
-
-api = Api()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
-api.init_app(app)
