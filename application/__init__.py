bfbfbf53005cbd5b8f05546936513f526e3eb13b
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-11-13 07:38:06 +0000

    Adds restplus to init

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from config import Config
 from flask_mongoengine import MongoEngine
+from flask_restplus import Api
 
+from config import Config
+
+api = Api()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
-
-from application import routes
+api.init_app(app)
