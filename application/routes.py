d430070246603e1b16064340630f464a9be21a9c
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-11-13 07:30:02 +0000

    Linting routes

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,17 @@
-from flask import Response, flash, json, redirect, render_template, request, url_for, session
+from flask import (
+    Response,
+    flash,
+    json,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 
 from application import app
 from application.forms import LoginForm, RegisterForm
-from application.models import User, Course, Enrollment
+from application.models import Course, Enrollment, User
 
 courseData = [
     {
@@ -52,7 +61,7 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if session.get('username'):
+    if session.get("username"):
         return redirect(url_for("index"))
 
     form = LoginForm()
@@ -63,13 +72,14 @@ def login():
         user = User.objects(email=email).first()
         if user and user.get_password(password):
             flash(f"{user.first_name}, you are successfully logged in!", "success")
-            session['user_id'] = user.user_id
-            session['username'] = user.first_name
+            session["user_id"] = user.user_id
+            session["username"] = user.first_name
             return redirect("/index")
         else:
             flash("Sorry, something went wrong.", "danger")
 
     return render_template("login.html", title="Login", form=form, login=True)
+
 
 @app.route("/logout")
 def logout():
@@ -77,22 +87,21 @@ def logout():
     session.pop("username", None)
     return redirect(url_for("index"))
 
+
 @app.route("/courses/")
 @app.route("/courses/<term>")
-def courses(term = None):
+def courses(term=None):
     if term is None:
         term = "Spring 2019"
 
     classes = Course.objects.order_by("courseID")
 
-    return render_template(
-        "courses.html", courseData=classes, courses=True, term=term
-    )
+    return render_template("courses.html", courseData=classes, courses=True, term=term)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if session.get('username'):
+    if session.get("username"):
         return redirect(url_for("index"))
 
     form = RegisterForm()
@@ -116,11 +125,9 @@ def register():
     return render_template("register.html", title="Register", form=form, register=True)
 
 
-
 @app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
-
-    if not session.get('username'):
+    if not session.get("username"):
         return redirect(url_for("login"))
 
     courseID = request.form.get("courseID")
@@ -128,63 +135,55 @@ def enrollment():
     user_id = session.get("user_id")
 
     if courseID:
-        if Enrollment.objects(user_id = user_id, courseID = courseID):
+        if Enrollment.objects(user_id=user_id, courseID=courseID):
             flash(f"Oops! You are already registered in {courseTitle}!", "danger")
             return redirect(url_for("courses"))
         else:
-            Enrollment(user_id = user_id, courseID = courseID).save()
+            Enrollment(user_id=user_id, courseID=courseID).save()
             flash(f"You are enrolled in {courseTitle}!", "success")
 
-    classes = list( User.objects.aggregate(*[
-            {
-                '$lookup': {
-                    'from': 'enrollment', 
-                    'localField': 'user_id', 
-                    'foreignField': 'user_id', 
-                    'as': 'r1'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$r1', 
-                    'includeArrayIndex': 'r1_id', 
-                    'preserveNullAndEmptyArrays': False
-                }
-            }, {
-                '$lookup': {
-                    'from': 'course', 
-                    'localField': 'r1.courseID', 
-                    'foreignField': 'courseID', 
-                    'as': 'r2'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$r2', 
-                    'preserveNullAndEmptyArrays': False
-                }
-            }, {
-                '$match': {
-                    'user_id': user_id
-                }
-            }, {
-                '$sort': {
-                    'courseID': 1
-                }
-            },
-    ]))
-
+    classes = list(
+        User.objects.aggregate(
+            *[
+                {
+                    "$lookup": {
+                        "from": "enrollment",
+                        "localField": "user_id",
+                        "foreignField": "user_id",
+                        "as": "r1",
+                    }
+                },
+                {
+                    "$unwind": {
+                        "path": "$r1",
+                        "includeArrayIndex": "r1_id",
+                        "preserveNullAndEmptyArrays": False,
+                    }
+                },
+                {
+                    "$lookup": {
+                        "from": "course",
+                        "localField": "r1.courseID",
+                        "foreignField": "courseID",
+                        "as": "r2",
+                    }
+                },
+                {"$unwind": {"path": "$r2", "preserveNullAndEmptyArrays": False}},
+                {"$match": {"user_id": user_id}},
+                {"$sort": {"courseID": 1}},
+            ]
+        )
+    )
 
     return render_template(
-        "enrollment.html",
-        enrollment=True,
-        title="Enrollment",
-        classes=classes
+        "enrollment.html", enrollment=True, title="Enrollment", classes=classes
     )
 
 
 @app.route("/api/")
 @app.route("/api/<idx>")
 def api(idx=None):
-    if idx == None:
+    if idx is None:
         jdata = courseData
     else:
         jdata = courseData[int(idx)]
